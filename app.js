8e529ccd26553e91962687989923c2cdb5425644
Eric Abell <ericabell@users.noreply.github.com>
2017-08-22 08:47:27 -0500

    Removed multiply

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ function subtract(a,b) {
     return a-b;
 }
 
-function multiply(a,b) {
-  return a*b;
-}
+
 
 console.log(`Sum of 2 and 3 is ${add(2,3)}`);
 console.log(`Difference of 5 and 2 is ${subtract(5,2)}`);
-console.log(`Product of 2 and 3 is ${multiply(2,3)}`);
