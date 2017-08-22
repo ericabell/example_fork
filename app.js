e879a9736b2b46e7ec3d10000e8a5048bbea0761
Eric Abell <ericabell@users.noreply.github.com>
2017-08-22 08:49:06 -0500

    Removed subtract operation

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ function add(a,b) {
     return a+b;
 }
 
-function subtract(a,b) {
-    return a-b;
-}
 
 
 
 console.log(`Sum of 2 and 3 is ${add(2,3)}`);
-console.log(`Difference of 5 and 2 is ${subtract(5,2)}`);
